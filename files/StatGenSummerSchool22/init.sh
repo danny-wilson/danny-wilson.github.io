bcecfceef27b941ff08fd69f610146cc12b66343
Danny Wilson <danny.wilson@gmail.com>
2022-11-23 16:28:28 +0000

    Updated initialization file for the updated kmer_pipeline:2022-10-26

--- files/StatGenSummerSchool22/init.sh
+++ files/StatGenSummerSchool22/init.sh
@@ -9,14 +9,35 @@ cd ~/Wednesday/amr
 
 # Download the pre-computed kmer_pipeline results
 echo 'Downloading pre-computed data for 992 Staphylococcus aureus genomes ...'
-#wget --quiet --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1uQ08isFiCKLQD7Ma7ABpcy6mZc7U7m-5' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1uQ08isFiCKLQD7Ma7ABpcy6mZc7U7m-5" -O saur.tar && rm -rf /tmp/cookies.txt && 
-wget --quiet https://git.ecdf.ed.ac.uk/hbecher/2206-osgss/-/raw/main/saur.tar && tar -xf saur.tar && rm saur.tar && chmod -R 755 take1/nucleotidekmer31_patternbatches && rm -rf take1/nucleotidekmer31_patternbatches
+DATA=https://www.well.ox.ac.uk/bioinformatics/training/whg_training_resources/data/genome_wide_association_studies/pathogen_gwas
+wget --quiet $DATA/saur.tar && tar -xf saur.tar && rm saur.tar && chmod -R 755 take1/nucleotidekmer31_patternbatches && rm -rf take1/nucleotidekmer31_patternbatches
 
 # Download the phenotypes
 echo 'Downloading antimicrobial resistance phenotypes for 992 S. aureus genomes ...'
-wget --quiet https://git.ecdf.ed.ac.uk/hbecher/2206-osgss/-/raw/main/pheno.tar -O pheno.tar && tar -xf pheno.tar && rm pheno.tar
+wget --quiet $DATA/pheno.tar -O pheno.tar && tar -xf pheno.tar && rm pheno.tar
 
 # Customize the prompt
 echo 'PS1="\[\e]0;\u@Singularity: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@Singularity\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ "' >> ~/.bashrc
 echo 'Done !'
 
+# Custom directory for scripts, to fix a bug in plotManhattanbowtie.Rscript
+mkdir -p ~/Wednesday/amr/scripts
+ln -s /usr/local/bin/* ~/Wednesday/amr/scripts/
+rm ~/Wednesday/amr/scripts/plotManhattanbowtie.Rscript
+sed 's/as.integer(args\[13\]))/as.integer(args\[13\])/g' /usr/local/bin/plotManhattanbowtie.Rscript > ~/Wednesday/amr/scripts/plotManhattanbowtie.Rscript
+sed -i 's/minor_allele_threshold = 0, ,/minor_allele_threshold = 0,/g' ~/Wednesday/amr/scripts/plotManhattanbowtie.Rscript
+chmod +x ~/Wednesday/amr/scripts/plotManhattanbowtie.Rscript
+
+# Install Entrez Direct
+cd ~
+sh -c "$(wget -q ftp://ftp.ncbi.nlm.nih.gov/entrez/entrezdirect/install-edirect.sh -O -)"
+
+# Download the S. aureus reference genome and genbank annotation
+~/edirect/esearch -db nucleotide -query "BX571856.1" | ~/edirect/efetch -format fasta > ~/Wednesday/amr/R00000022.fa
+~/edirect/esearch -db nucleotide -query "BX571856.1" | ~/edirect/efetch -format gb > ~/Wednesday/amr/R00000022.gbk
+
+# Rename the FASTA file to that used in the precomputed steps
+sed -i 's/BX571856.1/R00000022 BX571856.1/g' ~/Wednesday/amr/R00000022.fa
+
+# Update the pipeline software file
+sed 's$scriptpath\t/usr/local/bin$scriptpath\t/home/jovyan/Wednesday/amr/scripts$g' /usr/share/kmer_pipeline/example/pipeline_software_location.txt > ~/Wednesday/amr/pipeline_software_location.txt
